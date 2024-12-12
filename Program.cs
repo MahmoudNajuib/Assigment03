2acff64600bf58fc29be99b4d183da56f85e4c84
SHJuba <elprince.dokdok@gmail.com>
2024-12-12 02:06:34 +0200

    S

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, WWssWorld!");
+            Console.WriteLine("Hello, WWsssWorld!");
         }
     }
 }
