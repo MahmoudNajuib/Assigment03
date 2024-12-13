95d166acaa30b7f0f6a3b120a39ee6e08de5c4df
SHJuba <elprince.dokdok@gmail.com>
2024-12-13 03:03:50 +0200

    3.

--- Program.cs
+++ Program.cs
@@ -5,9 +5,120 @@
         static void Main(string[] args)
         {
 
-            #region Fracations & D
+            #region 1. Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no:
 
-            //float X = 4.5;
+            //Console.Write("Enter a Number: ");
+
+            //if (int.TryParse(Console.ReadLine(), out int Number))         //Passing by "Out".
+            //{
+            //    if (Number % 3 == 0 && Number % 4 == 0)
+            //    {
+            //        Console.WriteLine("Yes");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("No");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid Input. Please Enter a Valid Number.");
+            //}
+
+            #endregion
+
+            #region 2. Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive:
+
+            //Console.Write("Enter an Integer: ");
+
+            //if (int.TryParse(Console.ReadLine(), out int Number))
+            //{
+            //    if (Number < 0)
+            //    {
+            //        Console.WriteLine("Negative");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Positive");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid Input. Please Enter a Valid Integer.");
+            //}
+
+            #endregion
+
+            #region 3. Write a program that takes 3 integers from the user then prints the max Element and the min element:
+
+            //Console.WriteLine("Enter 3 Integers:\n");
+
+            //Console.Write("Write First number: ");
+            //int.TryParse(Console.ReadLine(), out int Num1);
+
+            //Console.Write("Second number: ");
+            //int.TryParse(Console.ReadLine(), out int Num2);
+
+            //Console.Write("Third number: ");
+            //int.TryParse(Console.ReadLine(), out int Num3);
+
+            //// Find the maximum and minimum values
+            //int Max = Math.Max(Num1, Math.Max(Num2, Num3));         //Froms Inside to Outside.
+            //int Min = Math.Min(Num1, Math.Min(Num2, Num3));         //Froms Outside to Inside.
+
+            //// Print results
+            //Console.WriteLine($"\nThe Max Element: {Max}");
+            //Console.WriteLine($"The Min Element: {Min}");
+
+            #endregion
+
+            #region 4. Write a program that allows the user to insert an integer number then check If a number is even or odd:
+
+
+            #endregion
+
+            #region 
+
+
+            #endregion
+
+            #region 
+
+
+            #endregion
+
+            #region 
+
+
+            #endregion
+
+            #region 
+
+
+            #endregion
+
+            #region 
+
+
+            #endregion
+
+            #region 
+
+
+            #endregion
+
+            #region 
+
+
+            #endregion
+
+            #region 
+
+
+            #endregion
+
+            #region 
+
 
             #endregion
 
